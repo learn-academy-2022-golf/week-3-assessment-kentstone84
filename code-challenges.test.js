fd67c10745cb0d5fb43296b5b16f7bf7aec076ae
Kent Stone <69402437+kentstone84@users.noreply.github.com>
2022-11-14 14:55:20 -0500

    Add files via upload

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -17,10 +17,35 @@
 const fibLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
-const fibLength2 = 10
+const fibLength2 = 10   
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+describe("fibonacci", () => {
+    test("returns an array of the fibonacci sequence", () => {
+        expect(fibonacci(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+        expect(fibonacci(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+})
+
 // b) Create the function that makes the test pass.
+
+//create a function that takes in a number
+function fibonacci(num) {
+    //create an array with the first two numbers of the fibonacci sequence
+    let fibArray = [1, 1]
+    //create a for loop that iterates through the array
+    for (let i = 2; i < num; i++) {
+        //add the previous two numbers in the array and push them into the array    
+        fibArray.push(fibArray[i - 2] + fibArray[i - 1])
+    }
+    //return the array
+    return fibArray
+}
+
+
+
+
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
@@ -38,6 +63,7 @@ const studyMinutesWeek1 = {
 }
 // Expected output: [15, 15, 20, 30, 30, 60, 90]
 
+
 const studyMinutesWeek2 = {
   sunday: 100,
   monday: 10,
@@ -49,7 +75,25 @@ const studyMinutesWeek2 = {
 }
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
+describe("sortStudyMinutes", () => {
+    test("returns an array of the values sorted from least to greatest", () => {
+        expect(sortStudyMinutes(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+        expect(sortStudyMinutes(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+    })
+})
 // b) Create the function that makes the test pass.
+
+//create a function that takes in an object
+function sortStudyMinutes(obj) {
+  //create a variable that stores the values of the object in an array
+  let studyMinutes = Object.values(obj)    // <--this is the Object.values() method referenced in the instructions// -->
+  //create a variable that stores the sorted array
+  let sortedMinutes = studyMinutes.sort((a, b) => a - b)
+  //return the sorted array
+  return sortedMinutes
+}
+
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
@@ -64,4 +108,30 @@ const accountTransactions2 = [250, -89, 100, -96]
 const accountTransactions3 = []
 // Expected output: []
 
+describe("accumulatingSum", () => {
+    test("returns an array of the accumulating sum", () => {
+        expect(accumulatingSum(accountTransactions1)).toEqual([100, 83, 60, 51])
+        expect(accumulatingSum(accountTransactions2)).toEqual([250, 161, 261, 165])
+        expect(accumulatingSum(accountTransactions3)).toEqual([])
+    })
+})
+
+
 // b) Create the function that makes the test pass.
+
+//create a function that takes in an array
+const accumulatingSum = (arr) => {
+    //create a variable that stores the sum of the array
+    let sum = 0
+    //create a variable that stores the new array
+    let newArr = []
+    //create a for loop that iterates through the array
+    for(let i = 0; i < arr.length; i++){
+        //add the sum of the array to the sum of the current index
+        sum += arr[i]
+        //push the sum into the new array
+        newArr.push(sum)
+    }
+}
+    //return the new array
+    return newArr
